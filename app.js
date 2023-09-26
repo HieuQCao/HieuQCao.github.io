723ab1dc3e1abd2014c71d62f853c04b3573de8e
Hieu Cao <146041645+HieuQCao@users.noreply.github.com>
2023-09-25 20:19:10 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+  //Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener("click", function () {
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
+      this.className += " active-btn";
+    });
+  }
+
+  //Sections active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //Remove the selected button from the other
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+
+  //Toggle theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
+}
+function downloadFile(event) {
+    event.preventDefault();
+    const fileUrl = 'https://drive.google.com/file/d/1-wocz6oVp9jjMxE1ILihILsXXjR9W8y0/view?usp=sharing';
+    const a = document.createElement('a');
+    a.href = fileUrl;
+    a.target = '_blank';
+    a.download = 'my-file.pdf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+  
+  
+
+PageTransitions();
