da2ce0859cf22ebffab9ab0f56368d090bf8c155
Hieu Cao <146041645+HieuQCao@users.noreply.github.com>
2023-10-06 23:45:44 -0400

    Add files via upload

--- send_email.php
+++ send_email.php
@@ -1,14 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $to = 'caoquanghieu2910@gmail.com';
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $message = $_POST["message"];
 
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+  $to = "caoquanghieu2910@gmail.com"; // Replace with your email address
+  $headers = "From: $name <$email>\r\n";
 
-    mail($to, $subject, $message, $headers);
+  if (mail($to, $subject, $message, $headers)) {
+    echo "Message sent successfully!";
+  } else {
+    echo "Error: Message not sent.";
+  }
 }
 ?>
